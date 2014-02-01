fe3c0f0384d827d70abb5f0393ba44b66250b81e
russm <russm@slofith.org>
2014-02-01 13:24:52 +1100

    cap sig, lower sig

---
+++ sha256.v
@@ -0,0 +1,35 @@
+module sha256_S0 (
+	input wire [31:0] x,
+	output wire [31:0] S0
+	);
+
+assign S0 = ({x[1:0], x[31:2]} ^ {x[12:0], x[31:13]} ^ {x[21:0], x[31:22]});
+
+endmodule
+
+module sha256_S1 (
+	input wire [31:0] x,
+	output wire [31:0] S1
+	);
+
+assign S1 = ({x[5:0], x[31:6]} ^ {x[10:0], x[31:11]} ^ {x[24:0], x[31:25]});
+
+endmodule
+
+module sha256_s0 (
+	input wire [31:0] x,
+	output wire [31:0] s0
+	);
+
+assign s0 = ({x[6:0], x[31:7]} ^ {x[17:0], x[31:18]} ^ {x[2:0], x[31:3]});
+
+endmodule
+
+module sha256_s1 (
+	input wire [31:0] x,
+	output wire [31:0] s1
+	);
+
+assign s1 = ({x[16:0], x[31:17]} ^ {x[18:0], x[31:19]} ^ {x[9:0], x[31:10]});
+
+endmodule
---
+++ sha512.v
@@ -0,0 +1,35 @@
+module sha512_S0 (
+	input wire [63:0] x,
+	output wire [63:0] S0
+	);
+
+assign S0 = ({x[27:0], x[63:28]} ^ {x[33:0], x[63:34]} ^ {x[38:0], x[63:39]});
+
+endmodule
+
+module sha512_S1 (
+	input wire [63:0] x,
+	output wire [63:0] S1
+	);
+
+assign S1 = ({x[13:0], x[63:14]} ^ {x[17:0], x[63:18]} ^ {x[40:0], x[63:41]});
+
+endmodule
+
+module sha512_s0 (
+	input wire [63:0] x,
+	output wire [63:0] s0
+	);
+
+assign s0 = ({x[0:0], x[63:1]} ^ {x[7:0], x[63:8]} ^ {x[6:0], x[63:7]});
+
+endmodule
+
+module sha512_s1 (
+	input wire [63:0] x,
+	output wire [63:0] s1
+	);
+
+assign s1 = ({x[18:0], x[63:19]} ^ {x[60:0], x[63:61]} ^ {x[5:0], x[63:6]});
+
+endmodule
